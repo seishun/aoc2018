d0c81cd5321793beca315aa1ff2a7b57a38a8eb2
Nikolai Vavilov <vvnicholas@gmail.com>
2018-12-01 16:39:04 +0200

    Add day1 part2

--- day1.hs
+++ day1.hs
@@ -1,6 +1,20 @@
+import Data.List
+import Data.Set (Set)
+import qualified Data.Set as Set
+
 parse :: String -> Int
 parse ('+' : xs) = read xs
 parse xs = read xs
 
+firstDuplicate :: Int -> Set Int -> [Int] -> Int
+firstDuplicate current seen (x:xs) =
+  let resulting = current + x
+  in if Set.member resulting seen
+  then resulting
+  else firstDuplicate resulting (Set.insert resulting seen) xs
+
 part1 :: String -> Int
 part1 = sum . map parse . lines
+
+part2 :: String -> Int
+part2 = firstDuplicate 0 Set.empty . cycle . map parse . lines
