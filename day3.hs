269dee81055db89ae1d05f7a26a81c97df52510f
Nikolai Vavilov <vvnicholas@gmail.com>
2018-12-03 15:51:11 +0200

    Add day3 part2

--- day3.hs
+++ day3.hs
@@ -1,22 +1,35 @@
-import Data.Map (Map)
+import Data.List
+import Data.Maybe
+import Data.Map (Map, (!))
 import qualified Data.Map as Map
 
-type Claim = (Int, Int, Int, Int)
+type Claim = (Int, Int, Int, Int, Int)
 
 addClaim :: Map (Int, Int) Int -> Claim -> Map (Int, Int) Int
-addClaim claims (left, top, width, height) = Map.unionWith (+) claims $ Map.fromList area
-  where area = do
-        x <- [left .. left + width - 1]
-        y <- [top .. top + height - 1]
-        return ((x, y), 1)
+addClaim areas = Map.unionWith (+) areas . Map.fromList . map (\s -> (s, 1)) . getArea
+
+getArea :: Claim -> [(Int, Int)]
+getArea (_, left, top, width, height) = do
+  x <- [left .. left + width - 1]
+  y <- [top .. top + height - 1]
+  return (x, y)
+
+overlaps :: Map (Int, Int) Int -> Claim -> Bool
+overlaps areas = any (\s -> areas ! s > 1) . getArea
 
 parse :: [String] -> Claim
-parse [_, "@", offsets, size] =
+parse ['#':id_, "@", offsets, size] =
   let [(left, ',':top_)] = reads offsets
       [(top, ":")] = reads top_ 
       [(width, 'x':height_)] = reads size
       height = read height_
-  in (left, top, width, height)
+  in (read id_, left, top, width, height)
 
 part1 :: String -> Int
 part1 = Map.size . Map.filter (>1) . foldl addClaim Map.empty . map (parse . words) . lines
+
+part2 :: String -> Int
+part2 input = id
+  where claims = map (parse . words) $ lines input
+        areas = foldl addClaim Map.empty claims
+        (id, _, _, _, _) = fromJust $ find (not . overlaps areas) claims
