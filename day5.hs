30e06fa09c824f8078144244c2c3f38796413c54
Nikolai Vavilov <vvnicholas@gmail.com>
2018-12-08 19:30:08 +0200

    Add day5 part2

--- day5.hs
+++ day5.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.List
 
 react :: String -> String
 react (u1:u2:xs)
@@ -7,8 +8,19 @@ react (u1:u2:xs)
   | otherwise = u1 : react (u2:xs)
 react xs = xs
 
+fullyReact :: String -> String
+fullyReact p = let p' = react p
+  in if p == p'
+  then p
+  else fullyReact p'
+
 part1 :: String -> Int
-part1 input = let input' = react input
-  in if input == input'
-  then length input
-  else part1 input'
+part1 = length . fullyReact . head . lines
+
+part2 :: String -> Int
+part2 input = let p = fullyReact $ head $ lines input
+  in minimum $ do
+    let units = nub $ map toUpper $ p
+    unit <- units
+    let p' = filter (\c -> c /= unit && c /= toLower unit) p
+    return (length $ fullyReact p')
