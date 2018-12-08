ddedbf88e3c3e900d744d9fefc6cfa52757f9015
Nikolai Vavilov <vvnicholas@gmail.com>
2018-12-08 18:52:56 +0200

    Add day4 part2

--- day4.hs
+++ day4.hs
@@ -26,13 +26,20 @@ process id (((_, _, _, start), Fall) : ((_, _, _, end), Wake) : xs) = Map.insert
         return (minute, 1)
 process _ [] = Map.empty
 
+parseInput :: String -> Map Int (Map Int Int)
+parseInput = process 0 . sortBy (comparing fst) . map (parseRecord . words) . lines
+
 mostAsleepGuard :: Map Int (Map Int Int) -> (Int, Map Int Int)
 mostAsleepGuard = maximumBy (comparing (Map.foldr (+) 0 . snd)) . Map.toList
 
-mostAsleepMinute :: Map Int Int -> Int
-mostAsleepMinute = fst . maximumBy (comparing snd) . Map.toList
+mostAsleepMinute :: Map Int Int -> (Int, Int)
+mostAsleepMinute = maximumBy (comparing snd) . Map.toList
 
 part1 :: String -> Int
 part1 input = guard * minute
-  where (guard, minutes) = mostAsleepGuard $ process 0 $ sortBy (comparing fst) $ map (parseRecord . words) $ lines input
-        minute = mostAsleepMinute minutes
+  where (guard, minutes) = mostAsleepGuard $ parseInput input
+        minute = fst $ mostAsleepMinute minutes
+
+part2 :: String -> Int
+part2 input = guard * minute
+  where (guard, (minute, _)) = maximumBy (comparing (fst . snd)) $ Map.toList $ Map.map mostAsleepMinute $ parseInput input
