f849af7cb224a8817fb85b27d5054b38894e7f79
Nikolai Vavilov <vvnicholas@gmail.com>
2018-12-17 20:53:26 +0200

    Add day15 part1

---
+++ day15.hs
@@ -0,0 +1,100 @@
+import Control.Monad
+import Data.List
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+data Class = Elf | Goblin deriving (Eq, Show)
+type Unit = (Class, Int)
+type Location = (Int, Int)
+type State = Map Location Unit
+
+parse' :: [(Int, Char)] -> (String, [(Int, Unit)])
+parse' ((i,l):xs) =
+  let (s, us) = parse' xs
+  in case l of
+    'E' -> ('.':s, (i, (Elf, 200)):us)
+    'G' -> ('.':s, (i, (Goblin, 200)):us)
+    _ -> (l:s, us)
+parse' [] = ("", [])
+
+parse :: String -> ([String], State)
+parse input =
+  let (ls, us) = unzip . map p . zip [0..] . map (parse' . zip [0..]) . lines $ input
+  in (ls, Map.fromList $ concat us)
+  where p (y, (l, us)) = (l, map (\(x, u) -> ((y, x), u)) us)
+
+adjacent :: [String] -> State -> Location -> [Location]
+adjacent cave state (y, x) = do
+  (y', x') <- [(y+1, x), (y-1, x), (y, x+1), (y, x-1)]
+  guard $ y' >= 0 && y' < length cave
+  guard $ x' >= 0 && x' < length (cave !! y')
+  guard $ cave !! y' !! x' == '.'
+  guard $ Map.notMember (y', x') state
+  return (y', x')
+
+bfs :: [String] -> State -> [Location] -> Set Location -> Set Location -> Maybe Int
+bfs cave state to visited unvisited =
+  if any (`elem` to) unvisited
+  then Just 0
+  else let unvisited' = Set.filter (`Set.notMember` visited) $ Set.fromList $ concatMap (adjacent cave state) unvisited
+           visited' = Set.union visited unvisited
+  in if Set.null unvisited'
+  then Nothing
+  else (1 +) <$> bfs cave state to visited' unvisited'
+
+move :: [String] -> [Location] -> (State, Location) -> (State, Location)
+move cave targets (state, (y, x)) =
+  if any (`elem` [(y+1,x), (y-1,x), (y,x+1), (y,x-1)]) targets
+  then (state, (y, x))
+  else let inRange = nub $ concatMap (adjacent cave state) targets
+           steps = sort $ do
+              (y', x') <- adjacent cave state (y, x)
+              distance <- maybeToList $ bfs cave state inRange Set.empty $ Set.singleton (y', x')
+              return (distance, (y', x'))
+  in case listToMaybe steps of
+    Nothing -> (state, (y, x))
+    Just (_, (y', x')) ->
+      let (Just unit, state') = Map.updateLookupWithKey (\_ _ -> Nothing) (y, x) state
+      in (Map.insert (y', x') unit state', (y', x'))
+
+attack :: [String] -> [Location] -> (State, Location) -> State
+attack cave targets (state, (y, x)) =
+  case sort $ catMaybes $ map inRange targets of
+    [] -> state
+    ((hp, target):_) ->
+      let hp' = hp - 3
+      in if hp' <= 0
+      then Map.delete target state
+      else Map.adjust (\(c, _) -> (c, hp')) target state
+  where inRange (y', x') =
+          if (y', x') `elem` [(y+1,x), (y-1,x), (y,x+1), (y,x-1)]
+          then Just (snd $ state Map.! (y', x'), (y', x'))
+          else Nothing
+
+next' :: [String] -> State -> [(Location, Unit)] -> (State, Bool)
+next' cave state (((y, x), unit@(c, _)):xs) =
+       let targets = Map.keys $ Map.filter ((/= c) . fst) state
+       in if null targets
+       then (state, True)
+       else let state' = attack cave targets $ move cave targets (state, (y, x))
+                xs' = filter ((`Map.member` state') . fst) xs
+            in next' cave state' xs'
+next' _ state [] = (state, False)
+
+next :: [String] -> State -> (State, Bool)
+next cave state = next' cave state $ Map.toList state
+
+outcome :: Int -> [String] -> State -> Int
+outcome rounds cave state =
+  let (state', end) = next cave state
+  in if end
+  then rounds * sum (map snd $ Map.elems state')
+  else outcome (rounds + 1) cave state'
+
+part1 :: String -> Int
+part1 input =
+  let (cave, state) = parse input
+  in outcome 0 cave state
