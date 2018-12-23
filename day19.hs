7a2e4adb0e51746eac50ce1ce1bdb415e678951d
Nikolai Vavilov <vvnicholas@gmail.com>
2018-12-23 11:54:29 +0200

    Add day19 part2

--- day19.hs
+++ day19.hs
@@ -12,16 +12,16 @@ get 3 [_, _, _, value, _, _] = value
 get 4 [_, _, _, _, value, _] = value
 get 5 [_, _, _, _, _, value] = value
 
-set :: Int -> Registers -> Int -> Registers
-set 0 [_, _1, _2, _3, _4, _5] value = [value, _1, _2, _3, _4, _5]
-set 1 [_0, _, _2, _3, _4, _5] value = [_0, value, _2, _3, _4, _5]
-set 2 [_0, _1, _, _3, _4, _5] value = [_0, _1, value, _3, _4, _5]
-set 3 [_0, _1, _2, _, _4, _5] value = [_0, _1, _2, value, _4, _5]
-set 4 [_0, _1, _2, _3, _, _5] value = [_0, _1, _2, _3, value, _5]
-set 5 [_0, _1, _2, _3, _4, _] value = [_0, _1, _2, _3, _4, value]
+set :: Int -> Int -> Registers -> Registers
+set 0 value [_, _1, _2, _3, _4, _5] = [value, _1, _2, _3, _4, _5]
+set 1 value [_0, _, _2, _3, _4, _5] = [_0, value, _2, _3, _4, _5]
+set 2 value [_0, _1, _, _3, _4, _5] = [_0, _1, value, _3, _4, _5]
+set 3 value [_0, _1, _2, _, _4, _5] = [_0, _1, _2, value, _4, _5]
+set 4 value [_0, _1, _2, _3, _, _5] = [_0, _1, _2, _3, value, _5]
+set 5 value [_0, _1, _2, _3, _4, _] = [_0, _1, _2, _3, _4, value]
 
 op :: (Int -> Registers -> Int) -> (Int -> Registers -> Int) -> (Int -> Int -> Int) -> Op
-op getA getB fun registers a b c = set c registers $ fun (getA a registers) (getB b registers)
+op getA getB fun registers a b c = set c (fun (getA a registers) (getB b registers)) registers
 
 parseOpcode :: String -> Op
 parseOpcode "addr" = op get get (+)
@@ -49,6 +49,9 @@ parseInstruction instruction =
 parse :: [String] -> (Int, [Instruction])
 parse (('#':'i':'p':' ':ip_):xs) = (read ip_, map parseInstruction xs)
 
+factors :: Int -> [Int]
+factors product = [factor | factor <- [1..product], product `mod` factor == 0]
+
 run :: Registers -> (Int, [Instruction]) -> Registers
 run registers (ip, instructions) =
   let ip' = get ip registers
@@ -56,7 +59,12 @@ run registers (ip, instructions) =
   then registers
   else let (op, a, b, c) = instructions !! ip'
            registers' = op registers a b c
-  in run (set ip registers' $ get ip registers' + 1) (ip, instructions)
+  in if ip' == 4
+  then run (set 0 (sum $ factors $ get b registers) $ set ip 16 registers) (ip, instructions)
+  else run (set ip (get ip registers' + 1) registers') (ip, instructions)
 
 part1 :: String -> Int
 part1 = get 0 . run [0,0,0,0,0,0] . parse . lines
+
+part2 :: String -> Int
+part2 = get 0 . run [1,0,0,0,0,0] . parse . lines
